d365eb300aeec438145c49efd39cc309a19004f2
Mimikyu <imlauraok@gmail.com>
2019-05-03 14:33:59 +0100

    Deactivating Menus

--- PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.h
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -18,6 +18,9 @@ class PUZZLEPLATFORMS_API UMainMenu : public UUserWidget
 public:
 	void SetMenuInterface(IMenuInterface* MenuInterface);
 
+	void Setup();
+	void Teardown();
+
 protected:
 	virtual bool Initialize() override;
 
